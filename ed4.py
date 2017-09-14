71e7e1c2495dcb633b9e100a6210d8d8ae9ceefe
irandijunior1903 <irandi.machado@dce.ufpb.br>
2017-09-14 09:59:21 -0300

    Decisão

---
+++ ed4.py
@@ -0,0 +1,6 @@
+letra = input("Digite uma letra: ")
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+    print("A letra {} é vogal!". format(letra))
+else:
+    print("A letra {} é consoante!". format(letra))
+print("FIM")
