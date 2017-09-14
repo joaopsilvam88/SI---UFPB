5a5bfcbd426094d448222cd7fad2c0c8da209afe
irandijunior1903 <irandi.machado@dce.ufpb.br>
2017-09-14 10:09:18 -0300

    Remover

--- ed18.py
+++
@@ -1,8 +0,0 @@
-dia = int(input("Digite um número correspondente ao dia: "))
-mes = int(input("Digite um número correspondente ao mes: "))
-ano = int(input("Digite um número correspondente ao ano: "))
-if dia > 31 or mes > 12:
-    print("Formato de data inválido!")
-else:
-    print("{} / {} / {}". format(dia, mes, ano))
-print("FIM")
--- ed22.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Digite um valor sem casas decimais(inteiro): "))
-if num%2==0:
-    print("O número {} é par!". format(num))
-else:
-    print("O número {} è ímpar!". format(num))
-print("FIM")
