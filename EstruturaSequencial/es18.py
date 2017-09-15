0a883f1723f0ab223b6727e2d47df136be60a165
joaopsilvam88 <31657979+joaopsilvam88@users.noreply.github.com>
2017-09-15 19:05:24 -0300

    Update es18.py

--- EstruturaSequencial/es18.py
+++ EstruturaSequencial/es18.py
@@ -1,7 +1,7 @@
 tamanhoArq = float(input("Quantos MB tem o arquivo a baixar: "))
-velocidadeD = float(input("Qual a velociade de um link de internet em Mbps: "))
+velocidadeD = float(input("Qual a velociade de um link de internet em Mbps: ")) 
 tempoD = (tamanhoArq / velocidadeD) / 60
 print("O tempo do download do seu arquivo será de %.2f minuto(s)" %tempoD)
 print("FIM")
     
-    
+ ####Só uma conferida nisso, são grandezas diferentes, Megabytes(MB) e Megabits(Mb). Megabytes são um conjunto de 8 Megabits.   
